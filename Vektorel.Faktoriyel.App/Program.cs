0f7b0347bbef417f19c980a5b3585e52a51753f3
İlker Ünver <108666062+IlkerUnver00@users.noreply.github.com>
2022-07-05 13:21:07 +0300

    Add files via upload

---
+++ Vektorel.Faktoriyel.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorel.Faktoriyel.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz:");
+            int sinir = Convert.ToInt32(Console.ReadLine());
+            int sonuc = 1;
+            for (int i = 1; i <= sinir; i++)
+            {
+                sonuc *= i;
+            }
+            Console.WriteLine("Faktoriyel Sonucu:"+sonuc);
+            Console.ReadKey();
+
+
+        }
+    }
+}
